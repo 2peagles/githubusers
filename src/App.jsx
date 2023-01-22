28819b746942797da024dae442d9f92d98d2a85b
Precious <precioud.d.bey@gmail.com>
2023-01-21 23:54:39 -0500

    body

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,59 @@
 import React,{useState} from 'react'
-import './App.scss'
-
+// import useLocalStorage from 'use-local-storage';
+import './index.scss'
 function App() {
  const[toggle,setToggle]=useState(false);
  const handleClick = () => setToggle(!toggle);
+ 
   return (
-    <div className={!toggle ? 'light':'dark'} id='app'>
-      <div className='center'>
-      <article>
-          <h1 className={!toggle ? 'light' : 'dark'} id='heading'>devfinder</h1>
-          <button onClick={handleClick}>  
-          {!toggle ? <span>night <img src='/assets/icon-moon.svg' alt='moon mode' /></span>
-            : <span>light<img src='/assets/icon-sun.svg' alt='light mode' /></span> 
-             }</button>
+    <div className='app'>
+      <div className='app_content'>
+      <article id='intro'>
+          <h1 id='heading'>devfinder</h1>
+          <button onClick={handleClick} className={!toggle ? ':root' : ''}>  
+            {!toggle ? <span> night <img src='/assets/icon-moon.svg' alt='moon mode' /></span> : <span> light <img src='/assets/icon-sun.svg' alt='light mode' /></span>}
+          </button>
       </article>
 
-      <article className={!toggle ? 'light' : 'dark'} id='card'>
-          <span><img src='/assets/icon-search.svg' alt='light mode' /><input placeholder=' Search Github username...' /></span>
+      <article>
+        <div>
+            <img src='/assets/icon-search.svg' alt='light mode' /><input placeholder=' Search Github username...' />
+        </div>
         <button>search </button>
       </article>
 
-      <article className={!toggle ? 'light' : 'dark'} id='card'>
-        <img src='' alt=''/>
+      <article  id='card'>
+        <img src='/assets/react.svg' alt=''/>
         <div>
-            <span className={!toggle ? 'light' : 'dark'}  id='heading'>name</span>
+          <div>
+              <span id='heading'>name</span>
+              <span>joined </span>
+              <a><p>profile link</p></a>
+              <p>bio</p>
+          </div>
+            {/* <span  id='heading'>name</span>
           <span>joined </span>
           <a><p>profile link</p></a>
-          <p>bio</p>
-          <div className='repo info'>
-            <span>repos</span>
-            <span>followers</span>
-            <span>following</span>
+          <p>bio</p> */}
+          <div className='repo'>
+            <div className='repo'>
+                <span>repos</span>
+                <span>repos</span>
+            </div>
+              <div className='follower'>
+                <span>follower</span>
+                <span>repos</span>
+              </div>
+              <div className='following'>
+                <span>folllowing</span>
+                <span>repos</span>
+              </div>
           </div>
           <div className='contact'>
             <span>location</span>
-            <a>twitter link</a>
+            <a>twitter </a>
             <a>link</a>
-            <a>proile link</a>
+            <a>proile </a>
           </div>
         </div>
       </article>
