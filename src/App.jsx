fd1d5b6d09b5999af66aebfff768458e187c97ba
Precious <precioud.d.bey@gmail.com>
2023-01-22 23:58:00 -0500

    fixed: body

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,13 @@ function App() {
  const handleClick = () => setToggle(!toggle);
  
   return (
-    <div className='app'>
+    <div id='app' className={!toggle ? 'dark' : 'light'}>
       <div className='app_content'>
       <article id='intro'>
-          <h1 id='heading'>devfinder</h1>
-          <button onClick={handleClick} className={!toggle ? ':root' : ''}>  
-            {!toggle ? <span> night <img src='/assets/icon-moon.svg' alt='moon mode' /></span> : <span> light <img src='/assets/icon-sun.svg' alt='light mode' /></span>}
+          <h1 className='heading'>devfinder</h1>
+          <button onClick={handleClick} className='toggle'>  
+            {!toggle ? <span className={!toggle ? 'dark' : 'light'} id='darktoggle'> <p>night</p><img src='/assets/icon-moon.svg' alt='moon mode' /></span> 
+            : <span className={!toggle ? 'dark' : 'light'} id='lighttoggle'> <p>light</p><img src='/assets/icon-sun.svg' alt='light mode' /></span>}
           </button>
       </article>
 
@@ -22,7 +23,43 @@ function App() {
         <button>search </button>
       </article>
 
-      <article  className='card'>
+
+        <article className='main'>
+          {/* <div className=''> */}
+            
+          <div className='main_intro'>
+              <img src='' alt='' className='profile' />
+              <div>
+            <p  className='heading'>name</p>
+            <p>joined </p>
+            <p><a>profile line</a></p>
+            <p>bio Lorem, ipsum dolor sit amet consectetur! Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
+          </div>
+          </div>
+          {/* </div> */}
+
+          <div className='main_content'>
+            <ol className='githubuser_data'>
+              <li>Repos
+                  <li>0</li>
+              </li>
+              <li>followers
+                  <li>0</li>
+              </li>
+              <li>following
+                  <li>0</li>
+              </li>
+            </ol>
+
+            <ol className='icon_content'>
+              <li><img src='/assets/icon-location.svg' alt='location' /> location </li>
+              <li><a> <img src='/assets/icon-website.svg' alt='link icon' /> link</a></li>
+              <li> <a><img src='/assets/icon-twitter.svg' alt='twitter link' /> twitter </a></li>
+              <li><a><img src='/assets/icon-company.svg' alt='building' /> proile </a></li>
+            </ol>
+          </div>
+        </article>
+        {/* <article className='card' >
         <div>
         <img src='/assets/react.svg' alt=''/>
           <div className='card_intro'>
@@ -60,8 +97,8 @@ function App() {
           </div>
         </div>
             </div>
-          </div>
-      </article>
+          </div> */}
+      {/* </article> */}
       </div>
     </div>
   )
