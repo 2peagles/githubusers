d45add7108880e74aa26bd8cdbce8fd803920d70
Precious <precioud.d.bey@gmail.com>
2023-01-23 00:07:17 -0500

    fixed:icons

--- src/App.jsx
+++ src/App.jsx
@@ -53,52 +53,12 @@ function App() {
 
             <ol className='icon_content'>
               <li><img src='/assets/icon-location.svg' alt='location' /> location </li>
+              <li> <a><img src='/assets/icon-twitter.svg' alt='twitter link' /> twitter </a></li>  
               <li><a> <img src='/assets/icon-website.svg' alt='link icon' /> link</a></li>
-              <li> <a><img src='/assets/icon-twitter.svg' alt='twitter link' /> twitter </a></li>
               <li><a><img src='/assets/icon-company.svg' alt='building' /> proile </a></li>
             </ol>
           </div>
         </article>
-        {/* <article className='card' >
-        <div>
-        <img src='/assets/react.svg' alt=''/>
-          <div className='card_intro'>
-              <span id='heading'>name</span>
-              <span>joined </span>
-              <a><p>profile link !</p></a>
-              <p>bio Lorem, ipsum dolor sit amet consectetur </p>
-            </div>
-          </div>
-
-          <div className='cards'>
-          <div className='card_subcontent'>
-            <div className='cards_info'>
-            <div className='repo'>
-                <span>repos</span>
-                <span>repos</span>
-            </div>
-              <div className='follower'>
-                <span>follower</span>
-                <span>repos</span>
-              </div>
-              <div className='following'>
-                <span>folllowing</span>
-                <span>repos</span>
-              </div>       
-              </div>
-          </div>
-          <div className='card_subcontent'>
-            <div className='cards_contact_info'>
-          <div className='contact'>
-            <span><img src='/assets/icon-location.svg' alt='location' /> location</span>
-                  <a><img src='/assets/icon-twitter.svg' alt='twitter link' /> twitter </a>
-                  <a> <img src='/assets/icon-website.svg' alt='link icon' /> link</a>
-                  <a><img src='/assets/icon-company.svg' alt='building' /> proile </a>
-          </div>
-        </div>
-            </div>
-          </div> */}
-      {/* </article> */}
       </div>
     </div>
   )
